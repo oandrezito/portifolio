b2d631ad05191857c8f1e0fac14b27e7103446f7
Andre <andrefernandolara@hotmail.com>
2024-02-18 18:22:53 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-content p-5 border-b ">
+          <code className="font-mono font-bold">oandrezito</code>
+        </p>
+      </div>
+
+    </main>
+  );
+}
