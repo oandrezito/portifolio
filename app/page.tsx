4c0b1360fa4d16d1b03990f71af8172cbc436a84
Andre Fernando Lara <andrefernandolara@hotmail.com>
2024-02-19 17:09:41 -0300

    add components: HeroSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import HeroSection from "./components/HeroSection";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-content p-5 border-b ">
-          <code className="font-mono font-bold">oandrezito</code>
-        </p>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
       </div>
 
     </main>
